3dae1bb4780c73821e9c4ac8a44a1cc6c0c2f5b1
Dewey <hammersdrichard@gmail.com>
2021-06-27 09:35:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+const map = (array, func) => {
+  return array.map(num => func(num))
+}
+
+const reduce = (array, func, startingPoint) => {
+  if (!startingPoint){
+    return array.reduce((e, memo) => func(e, memo))
+  } else {
+    return array.reduce((e, memo) => func(e, memo), startingPoint)
+  }
+}
